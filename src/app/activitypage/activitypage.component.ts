ef4e59e26f328afd9b8ad327af0dccca1481a251
Avantika <singh_avantika@yahoo.com>
2022-01-29 19:04:17 -0600

    activitypage component added and bootstrap installed

---
+++ src/app/activitypage/activitypage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activitypage',
+  templateUrl: './activitypage.component.html',
+  styleUrls: ['./activitypage.component.css']
+})
+export class ActivitypageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
